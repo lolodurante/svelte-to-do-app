8a3f51bade794dae674d6191acfe5eb201fe3f9d
Lorenzo Durante <lorenzodurante@MacBook-Air-de-Lorenzo.local>
2023-10-26 18:17:35 -0300

    Simple to do in svelte

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">  
+  let todos = [{
+    id: 1,
+    text: 'Learn Svelte'
+  }];
+  let inputValue: string;
+  const saveInputValue = () => {
+    todos = [...todos, {
+      id: todos.length + 1,
+      text: inputValue
+    }]
+    inputValue = ''
+  }
+  const handleDeleteItem = (id) => {
+    todos = todos.filter(todo => todo.id !== id)
+    todos = todos
+  }
+</script>
+
+<main>
+  <div>
+    <input type="text" bind:value={inputValue} placeholder="Add item to the to do">
+    <button on:click={saveInputValue}>Submit</button>
+  </div>
+  <ul>
+    {#each todos as todo}
+      <li id={String(todo.id)}>
+        <span class="text-item">{todo.text}</span>
+        <button on:click={() => handleDeleteItem(todo.id)}>Delete</button>
+      </li>
+      
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    margin: 20px;
+  } 
+  input {
+    width: 60vw;
+  }
+  ul {
+    width: 60%;
+    padding: 10px;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 15px;
+  }
+  .text-item {
+    margin-right: 10px;
+  }
+</style>
